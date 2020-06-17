2389c068f5b99dc05a5238b6399eabcc9f59ff1e
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-17 15:22:08 -0500

    Set up inner functions assignment test failing.

---
+++ more_functions/inner_functions_assignment.py
@@ -0,0 +1,21 @@
+"""
+Program: inner_functions_assignment.py
+Author: Paul Ford
+Last date modified: 06/17/2020
+Purpose: accepts a list of measurements
+         for a rectangle and returns a
+         string with perimeter and area
+"""
+
+
+def measurements(rect):
+    def area(a_list):  # calculates the area
+        pass
+
+    def perimeter(a_list):  # calculates the perimeter
+        pass
+
+    area = (rect)
+    perimeter = perimeter(rect)
+
+    return 'Perimeter = {}'
