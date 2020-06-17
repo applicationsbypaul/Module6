483e2b665d692816c1c228226a449652398a44bc
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-16 23:14:59 -0500

    Re wrote the first test. I was confused on how it should work. Like if you dont put the optional, do you still ask for input or do you just say Name: 0 to show default

--- more_functions/validate_input_in_functions.py
+++ more_functions/validate_input_in_functions.py
@@ -1,22 +1,24 @@
 def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
+    # if test score is default we will ask until
+    # we get valid data
     if test_score != 0:
         while True:
             try:
-                test_score = int(input('Please enter your test score: '))
+                test_score = int(test_score)
+                if int(test_score < 0) or int(test_score > 100):
+                    raise ValueError
+                    continue
             except ValueError:
                 print(invalid_message)
+                test_score = int(input('Please enter your test score: '))
                 continue
-            if int(test_score < 0) or int(test_score > 100):
-                print(invalid_message)
-                continue
-            else:
-                break
+            break
     # return {test_name: test_score}
     return test_name + ': ' + str(test_score)
 
 
 if __name__ == '__main__':
-    print(score_input('Paul'))
+    print(score_input('Paul', 50))
 
 # if not test_score.isdigit():
 # print(invalid_message)
